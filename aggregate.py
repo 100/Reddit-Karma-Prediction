5386dfb55c5d7542b1671c23e7e579cd4e2f7d9b
Devin <devinsoni1010@gmail.com>
2016-01-06 15:44:06 -0500

    web work done, not yet hosted

--- aggregate.py
+++ aggregate.py
@@ -1,6 +1,6 @@
 from sklearn.cluster import KMeans
 import numpy
-import os, random, json
+import os, random, json, operator, math
 try:
    import cPickle as pickle
 except:
@@ -9,16 +9,18 @@ except:
 
 '''
 Compute average karma per comment in each subreddit present in the training set,
-and save resulting dictionary to disk.
+and save resulting dictionary to disk. May be very time-consuming, so includes
+verbosity option.
 Args:
     path (string): file path to directory of training data
     skipRate (int) [optional]: use every $skipRate comment in the training data
-    minimum (int) [optional]: subreddit must have $minimum comments in total to
-        be included in final dictionary
+    top (int) [optional]: only use the top $top percent of subreddits ordered by
+        total number of comments
+    verbose (boolean) [optional]: print status of current progress
 Returns:
     subredditAverages (dict): {subreddit: average karma} pairs
 '''
-def computeAverages(path, skipRate = 1, minimum = 25):
+def computeAverages(path, skipRate = 1, top = 10, verbose = True):
     subredditScores = {}
     subredditNumComs = {}
     totalKarma = 0
@@ -42,62 +44,96 @@ def computeAverages(path, skipRate = 1, minimum = 25):
                         subredditScores[comment['subreddit']] = comment['score']
                     totalKarma += comment['score']
                     numComments += 1
-                print 'Completed ' + str(numComments) + ' comments.'
+                if verbose == True:
+                    print 'Completed ' + str(numComments) + ' comments.'
     print str(totalKarma) + ' total karma across all comments.'
     print str(numComments) + ' comments analyzed.'
     subredditAverages = {}
     for subreddit, score in subredditScores.iteritems():
-        if subredditNumComs[subreddit] > 25:
-            subredditAverages[subreddit]= float(score) /
-                subredditNumComs[subreddit]
+        subredditAverages[subreddit]= float(score) / subredditNumComs[subreddit]
+    sortedNumComs = sorted(subredditNumComs.items(),
+        key = operator.itemgetter(1))
+    cutoff = int((len(subredditNumComs)) * (float(top)/100))
+    cutoffNumComs = sortedNumComs[-cutoff:]
+    subredditAverages = dict([(subreddit[0], subredditAverages[subreddit[0]])
+        for subreddit in cutoffNumComs])
     with open('pickles/subredditAverages.pkl', 'wb') as pickleFile:
         pickle.dump(subredditAverages, pickleFile, pickle.HIGHEST_PROTOCOL)
-    return subredditAverages
+    with open('pickles/cutoffNumComs.pkl', 'wb') as pickleFile:
+        pickle.dump(dict(cutoffNumComs), pickleFile, pickle.HIGHEST_PROTOCOL)
+    return dict(cutoffNumComs), subredditAverages
 
 
 '''
-Creates dict representation of data needed for alchemy.js graph representation,
-and saves to disk.
+Creates dict representation of data needed for cytoscape.js graph
+representation, and saves to disk.
 Args:
-    skipRate (int) [optional]: only use every $skipRate subreddit
-    edgesPerNodes (int) [optional]: $edgesPerNode edges in each subreddit node
+    edgesPerNodes (int) [optional]: $edgesPerNode edges in each subreddit node to each other;
+        provide -1 to create densely-connected clusters
 Returns:
-    clusterDict (dict): JSON representation of data needed for alchemy.js
+    clusterDict (dict): JSON representation of data needed for cytoscape.js
 '''
-def createClusters(skipRate = 15, edgesPerNode = 3):
+def createClusters(edgesPerNode = 0):
     with open('pickles/subredditAverages.pkl', 'rb') as pickleFile:
         averagesDict = pickle.load(pickleFile)
+    with open('pickles/cutoffNumComs.pkl', 'rb') as pickleFile:
+        numComs = pickle.load(pickleFile)
     kmeans = KMeans(
         n_clusters = 5,
         verbose = 2
     )
     kmeans.fit(numpy.array(averagesDict.values()).reshape(-1, 1))
-    clusterDict = {'nodes':[]}
+    elements = []
     idCounter = 1
     for subreddit, avg in averagesDict.iteritems():
-        clusterDict['nodes'].append({
-            'id': idCounter,
-            'sub': subreddit,
-            'average': avg,
-            'cluster': kmeans.predict([avg])[0]
-        })
+        elements.append({
+            'data': {
+                'id': idCounter,
+                'sub': subreddit,
+                'average': round(avg, 3),
+                'cluster': kmeans.predict([avg])[0],
+                'size': int(15 * math.log(numComs[subreddit], 1000))
+            },
+            'position': {
+              'x': kmeans.predict([avg])[0]*400 + random.randint(-250, 250),
+              'y': random.randint(0, 700)
+        }})
         idCounter += 1
-    clusterDict['nodes'] = clusterDict['nodes'][::skipRate]
     clusterList = [[] for num in xrange(0, 5)]
-    for subreddit in clusterDict['nodes']:
-        id = subreddit['id']
-        cluster = subreddit['cluster']
+    for subreddit in elements:
+        id = subreddit['data']['id']
+        cluster = subreddit['data']['cluster']
         clusterList[cluster].append(id)
-    clusterDict['edges'] = []
     for cluster in clusterList:
         for subreddit in cluster:
-            targets = [random.randint(0, len(cluster) - 1) for num in
-                xrange(edgesPerNode)]
+            if edgesPerNode != -1:
+                targets = [random.randint(0, len(cluster) - 1) for num in
+                    xrange(edgesPerNode)]
+            else:
+                targets = [num for num in xrange(len(cluster))]
             for target in targets:
-                clusterDict['edges'].append({
+                elements.append({'data': {
                     'source': subreddit,
                     'target': cluster[target]
-                })
-    with open('pickles/nodesDictLarge.pkl', 'wb') as pickleFile:
-        pickle.dump(clusterDict, pickleFile, pickle.HIGHEST_PROTOCOL)
-    return clusterDict
+                }})
+    for idx, cluster in enumerate(clusterList):
+        elements.append({
+            'data': {
+                'id': 'cluster' + str(idx),
+                'sub': 'cluster' + str(idx),
+                'average': kmeans.cluster_centers_[idx][0],
+                'cluster': idx,
+                'size': 35
+            },
+            'position': {
+              'x': idx*400 + random.randint(-50,50),
+              'y': random.randint(300, 400)
+        }})
+        for sub in cluster:
+            elements.append({'data': {
+                'source': 'cluster' + str(idx),
+                'target': sub
+            }})
+    with open('pickles/elements.pkl', 'wb') as pickleFile:
+        pickle.dump(elements, pickleFile, pickle.HIGHEST_PROTOCOL)
+    return elements
--- app.py
+++ app.py
@@ -1,54 +1,115 @@
-from flask import Flask, render_template, request, jsonify, make_response
+from flask import Flask, render_template, request, jsonify, make_response, \
+    flash, abort
+from wtforms import Form, TextAreaField, validators
 from flask_limiter import Limiter
+from createFullClassifier import vectorize
+import json
 try:
    import cPickle as pickle
 except:
    import pickle
 
 app = Flask(__name__)
+app.secret_key = 'many random bytes'
 limiter = Limiter(app)
+
+class CommentForm(Form):
+    comment = TextAreaField('comment',
+        [validators.required(), validators.Length(min = 2, max = 1000)])
+
 
 def validateRequest(type, text):
     if type not in ['binary', 'bins']:
         return False
-    if type(text) not in [unicode, str] or len(text) <= 0 or len(text) > 1000:
+    if not isinstance(text, unicode) or len(str(text)) < 2 or len(str(text)) > 1000:
         return False
     return True
 
-
-def featureWeights(binClf, comment):
+def classifyComment(type, text):
+    if type == 'bins':
+        with open('pickles/fullClassifier.pkl', 'rb') as pickleFile:
+            fullClf = pickle.load(pickleFile)
+        with open('pickles/ngramBinaryClf.pkl', 'rb') as pickleFile:
+            ngram = pickle.load(pickleFile)
+        with open('pickles/blobber.pkl', 'rb') as pickleFile:
+            blobber = pickle.load(pickleFile)
+        with open('pickles/swearList.pkl', 'rb') as pickleFile:
+            swears = pickle.load(pickleFile)
+        vector = vectorize(blobber, text, ngram)
+        prediction = fullClf.predict([vector])[0]
+        return vector, prediction
+    if type == 'binary':
+        with open('pickles/ngramBinaryClf.pkl', 'rb') as pickleFile:
+            ngram = pickle.load(pickleFile)
+        prediction = ngram.predict([text])[0]
+        return prediction
 
 
 @app.route('/')
 def index():
-    pass
+    return render_template('index.html')
 
-@app.route('/api'):
+@app.route('/try', methods=['GET', 'POST'])
+def tryIt():
+    form = CommentForm(request.form)
+    if request.method == 'POST':
+        if form.validate():
+            comment = form.comment.data
+            vector, prediction = classifyComment('bins', comment)
+            if vector[9] == 1:
+                vector[9] == 'positive'
+            else:
+                vector[9] == 'negative'
+            return render_template('try.html', form = form, vector = vector,
+                classification = prediction)
+        else:
+            flash('Error! Hint: Comment must be between 2 and 1000 characters.')
+    return render_template('try.html', form = form)
+
+@app.route('/visualize')
+def visualize():
+    with open('pickles/elements.pkl', 'rb') as pickleFile:
+        elements = pickle.load(pickleFile)
+    return render_template('visualize.html', elements = json.dumps(elements))
+
+@app.route('/apidocs')
+def apidocs():
+    return render_template('apidocs.html')
+
+
+@app.route('/api')
+@limiter.limit("10 per minute")
+def api():
     if request.args is None:
         abort(400)
-    if request.args.get('type') not None:
-        clfType = request.args.get('type') #binary or bins
+    if request.args.get('type') is not None:
+        clfType = request.args.get('type')
     else:
         abort(400)
-    if request.args.get('text') not None:
+    if request.args.get('text') is not None:
         text = request.args.get('text')
     else:
         abort(400)
     if validateRequest(clfType, text) == False:
         abort(400)
-
-    ####
-    # CLASSIFY TEXT BASED ON SELECTED CLASSIFIER
-    ####
-
-    ####
-    # RETURN JSON RESPONSE WITH STATUS, KARMA PREDICTION RANGE
-    ####
+    text = str(text)
+    if clfType == 'bins':
+        __, prediction = classifyComment('bins', text)
+    else:
+        prediction = classifyComment('binary', comment)
+    status = {'status': 'ok', 'type': clfType, 'prediction': prediction}
+    return jsonify(status)
 
 @app.errorhandler(400)
 def incorrect(error):
-    return make_response(jsonify({'status': 'Error',
+    return make_response(jsonify({'status': 'error',
         'error': 'Incorrect or inappropriate parameters provided.'}), 400)
 
+@app.errorhandler(429)
+def ratelimitExceeded(error):
+    return make_response(jsonify({'status': 'error',
+        'error': 'Rate limit exceeded.'}), 429)
+
+
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=False)
